09e052d8a175546203b1f65395f8ac90423de730
Ahmed Swerky <ahmdswerky@gmail.com>
2022-10-01 09:55:42 +0200

    chore: replace jest setup file configurations

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	moduleDirectories: ['node_modules'],
 	moduleFileExtensions: ['js'],
 	roots: ['<rootDir>'],
-	setupFiles: ['./test/setup.js'],
+	setupFiles: ['./jest.setup.js'],
 	testMatch: ['**/?(*.)+(spec|test).js?(x)'],
 	testPathIgnorePatterns: ['/node_modules/'],
 	verbose: true,
